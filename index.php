4d0dbe2cfceed7a9db117de52d1ca1ca4b7ce438
saumik8763 <oracle.saumik@gmail.com>
2020-06-12 23:15:56 +0530

    added cloudfront url

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ print `ifconfig`
 ?>
 </pre>
 <html>
-<img align="middle" src='image.png' width='400' height='400' />
+<img align="middle" src='d2trar1c7f2392.cloudfront.net/image.jpg' width='400' height='400' />
 </html>
